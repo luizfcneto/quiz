82b6e2414174ab2735dd3cace85f816c0b73f449
luizfcneto <luizfcneto123@gmail.com>
2023-05-12 12:51:12 -0300

    +new page quiz, *new route /quiz created

---
+++ client/src/pages/Quiz.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="quiz-container">
+        <h1> Quiz Page </h1>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "Quiz",
+}
+</script>
+
+
+<style scoped>
+
+
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,22 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import Quiz from "../pages/Quiz.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: "/",
+      name: "HelloWorld",
       component: HelloWorld
+    },
+    {
+      path: "/quiz",
+      name: "Quiz",
+      component: Quiz
     }
-  ]
-})
+  ],
+  mode: "history"
+});
