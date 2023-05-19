dc21c99a39f33fada26769b106fb6f0afa9c2350
luizfcneto <luizfcneto123@gmail.com>
2023-05-19 01:13:38 -0300

    *calling Form component on Quiz page

--- client/src/pages/Quiz.vue
+++ client/src/pages/Quiz.vue
@@ -1,18 +1,25 @@
 <template>
     <div class="quiz-container">
         <h1> Quiz Page </h1>
+        <Form></Form>
     </div>
 </template>
 
 
 <script>
+import Form from "../components/modules/quiz/Form.vue";
 export default {
     name: "Quiz",
+    components: {
+        Form
+    }
 }
 </script>
 
 
-<style scoped>
-
+<style>
+.quiz-container{
+    background-color: #F6F1F1;
+}
 
 </style>
