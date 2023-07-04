b8ccf61d6a945890ae289d34b538e8de793bdcc2
elmojuh <junior.fool.skull@gmail.com>
2023-07-04 13:30:26 -0300

    fazendo mudanças em arquivo

--- client/src/components/modules/createdQuestion/CreatedQuestion.vue
+++ client/src/components/modules/createdQuestion/CreatedQuestion.vue
@@ -1,14 +1,21 @@
 <template>
     <div class="component">
         <div class="tipo">
-            <div class="h1">Qual o tipo de pergunta</div>
-            <select name="type" id="type" v-model="selectedType">
-                <option value="text">Texto</option>
-                <option value="textArea">Texto em Area</option>
-                <option value="radio">Radio/Bolinha</option>
-                <option value="checkbox">Check Box/Marcador</option>
-                <option value="range">Range</option>
-            </select>
+            <div class="box">
+                <!-- Renderização do tipo 'Texto' -->
+                <label class="h1">Pergunta aqui:</label>
+                <textarea />
+            </div>
+            <div class="box2">
+                <div class="h1">Qual o tipo de resposta</div>
+                <select name="type" id="type" v-model="selectedType">
+                    <option value="text">Texto</option>
+                    <option value="textArea">Texto em Area</option>
+                    <option value="radio">Radio/Bolinha</option>
+                    <option value="checkbox">Check Box/Marcador</option>
+                    <option value="range">Range</option>
+                </select>
+            </div>
         </div>
 
         <div class="box">
@@ -21,26 +28,26 @@
 
             <div v-if="selectedType === 'text'">
                 <!-- Renderização do tipo 'Texto' -->
-                <label for="textQuestion">Pergunta de Texto:</label>
+                <label for="textQuestion">Resposta em Texto:</label>
                 <input type="text" id="textQuestion" />
             </div>
 
             <div v-if="selectedType === 'textArea'">
                 <!-- Renderização do tipo 'Texto' -->
-                <label for="textQuestion">Pergunta de Texto:</label>
+                <label for="textQuestion">Resposta em Texto Área:</label>
                 <textarea />
             </div>
 
             <div v-if="selectedType === 'range'">
                 <!-- Renderização do tipo 'Range' -->
-                <label for="rangeQuestion">Pergunta de Range:</label>
+                <label for="rangeQuestion">Resposta em Range:</label>
                 <input type="range" id="rangeQuestion" v-model="rangeValue" />
                 <p>Valor selecionado: {{ rangeValue }}</p>
             </div>
 
             <div v-else-if="selectedType === 'radio'">
                 <!-- Renderização do tipo 'Radio/Bolinha' -->
-                <label>Pergunta de Radio:</label>
+                <label>Resposta em Radio:</label>
                 <input type="radio" name="radioQuestion" value="option1" />
                 Opção 1
                 <input type="radio" name="radioQuestion" value="option2" />
@@ -49,7 +56,7 @@
 
             <div v-else-if="selectedType === 'checkbox'">
                 <!-- Renderização do tipo 'Check Box/Marcar' -->
-                <label>Pergunta de Check Box:</label>
+                <label>Resposta em Check Box:</label>
                 <input
                     type="checkbox"
                     name="checkboxQuestion"
@@ -82,18 +89,22 @@ export default {
 <style scoped>
 .component {
     display: flex;
-    background-color: blue;
-    height: 200px;
+    background-color: rgb(94, 94, 250);
+    height: 300px;
 }
 
 .tipo {
     padding: 30px;
-    background-color: rgb(7, 196, 123);
+    background-color: rgb(83, 206, 159);
 }
 
 .box {
     padding: 30px;
-    background-color: rgb(245, 6, 253);
+    background-color: rgb(255, 172, 192);
+}
+.box2 {
+    padding: 30px;
+    background-color: rgb(170, 255, 195);
 }
 
 .negrito {
