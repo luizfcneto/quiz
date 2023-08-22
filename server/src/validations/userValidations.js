80bfcc9de24079b444caf9a7468f0fbcd93dad74
luizfcneto <luizfcneto123@gmail.com>
2023-08-22 01:22:07 -0300

    *refactoring userValidations

--- server/src/validations/userValidations.js
+++ server/src/validations/userValidations.js
@@ -1,23 +1,60 @@
 
-export const validateUser = (user) => {
-    console.log("Validando User");
+export const validateCreateUser = (user) => {
+    console.log("Validando Create User");
+    validateUser(user);
+    validateUserName(user);
+    validateUserEmail(user);
+    validateUserPassword(user);
+    validateUserConfirmPassword(user);
+}
 
+export const validateUpdateUser = (user) => {
+    console.log("Validando Update User");
+    validateUser(user);
+    validateUserId(user);
+    validateUserName(user);
+    validateUserEmail(user);
+    validateUserPassword(user);
+    validateUserConfirmPassword(user);
+}
+
+const validateUserId = (user) => {
+    if(!user.id) {
+        throw new Error("Bad Request, missing information");
+    }
+}
+
+const validateUser = (user) => {
+    console.log("Validando corpo da requisição user");
     if(!user){
         throw new Error("Bad Request, missing information");
     }
+}
 
+const validateUserName = (user) => {
+    console.log("Validando nome de usuario");
     if(!user.name){
         throw new Error("Bad Request, missing information");
     }
+}
 
+const validateUserEmail = (user) => {
+    console.log("Validando email de user");
     if(!user.email){
         throw new Error("Bad Request, missing information");
     }
+}
 
+const validateUserPassword = (user) => {
+    console.log("Validando senha de user");
     if(!user.password){
         throw new Error("Bad Request, missing information");
     }
 
+}
+
+const validateUserConfirmPassword = (user) => {
+    console.log("Validando confirmPassword de user");
     if(!user.confirmPassword){
         throw new Error("Bad Request, missing information");
     }
@@ -25,5 +62,4 @@ export const validateUser = (user) => {
     if(user.password !== user.confirmPassword){
         throw new Error("Bad Request, missing information");
     }
-
 }
